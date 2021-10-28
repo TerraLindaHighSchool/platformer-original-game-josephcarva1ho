f2fd938b5d9fed55b996a602e0c84d401d523c3e
josephcarva1ho <89489503+josephcarva1ho@users.noreply.github.com>
2021-10-28 14:49:16 -0700

    Completed

--- Level2.java
+++ Level2.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Level2 extends World
 {
     private final float GRAVITY = 0.0667f;
-    private final GreenfootSound MUSIC = new GreenfootSound("Level3.wav");
+    private final GreenfootSound MUSIC = new GreenfootSound("Background Music.wav");
     private final int SPEED = 3;
     private final float JUMP_FORCE = 5.6f;
     private final int MAX_HEALTH = 3;
